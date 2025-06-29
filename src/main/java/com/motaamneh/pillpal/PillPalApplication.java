8a4421ec6e2e97995f1857ae6403b2247524387e
motaamneh <mtaamneh292@gmail.com>
2025-06-29 18:47:33 +0300

    first commit

---
+++ src/main/java/com/motaamneh/pillpal/PillPalApplication.java
@@ -0,0 +1,13 @@
+package com.motaamneh.pillpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PillPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motaamneh/pillpal/PillPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motaamneh.pillpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
