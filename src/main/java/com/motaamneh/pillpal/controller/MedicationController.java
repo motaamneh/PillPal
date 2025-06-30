20931a8707d481922f71bb955ec3602c80651656
motaamneh <mtaamneh292@gmail.com>
2025-06-30 15:10:46 +0300

    MedicationController + changes on the service

---
+++ src/main/java/com/motaamneh/pillpal/controller/MedicationController.java
@@ -0,0 +1,35 @@
+package com.motaamneh.pillpal.controller;
+
+import com.motaamneh.pillpal.entity.Medication;
+import com.motaamneh.pillpal.service.MedicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/api/medications")
+@RequiredArgsConstructor
+public class MedicationController {
+    private final MedicationService medicationService;
+
+    @GetMapping("/user/{userId}")
+    public List<Medication> getMedicationsByUser(@PathVariable Long userId) {
+        return medicationService.getMedicationsByUserId(userId);
+
+    }
+    @PostMapping("/user/{userId}")
+    public Medication createMedication(@PathVariable Long userId, @RequestBody Medication medication) {
+        return medicationService.createMedication(userId,medication);
+    }
+    @PutMapping("/{id}")
+    public Medication updateMedication(@PathVariable Long id, @RequestBody Medication medication) {
+        return medicationService.updateMedication(id,medication);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteMedication(@PathVariable Long id) {
+        medicationService.deleteMedication(id);
+    }
+
+
+}
