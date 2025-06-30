bac3af337fc35646d830decdab2f1aad23f1897f
motaamneh <mtaamneh292@gmail.com>
2025-06-30 13:07:30 +0300

    MedicationService

--- src/main/java/com/motaamneh/pillpal/service/MedicationService.java
+++ src/main/java/com/motaamneh/pillpal/service/MedicationService.java
@@ -5,6 +5,7 @@ import com.motaamneh.pillpal.repository.MedicationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedicationService {
@@ -12,7 +13,36 @@ public class MedicationService {
     public MedicationService(MedicationRepository medicationRepository) {
         this.medicationRepository = medicationRepository;
     }
-//    public List<Medication> getUserMedications(Long userId) {
-//       return medicationRepository.findByUserId();
-//    }
+
+    public  Medication createMedication(Medication medication) {
+        return medicationRepository.save(medication);
+    }
+    public List<Medication> getAllMedications() {
+        return medicationRepository.findAll();
+    }
+    public List<Medication> getMedicationsByUserId(Long userId) {
+        return medicationRepository.findByUserId(userId);
+    }
+
+
+    public Optional<Medication> getUserMedicationById(Long id) {
+       return medicationRepository.findById(id);
+    }
+
+    public Medication updateMedication(Long id,Medication updatedMedication) {
+        return medicationRepository.findById(id)
+                .map( medication -> {
+                    medication.setName(updatedMedication.getName());
+                    medication.setDosage(updatedMedication.getDosage());
+                    medication.setDescription(updatedMedication.getDescription());
+                    medication.setUser(updatedMedication.getUser());
+
+                    return medicationRepository.save(medication);
+                        }
+                ).orElseThrow(()-> new RuntimeException("Medication is not found"));
+    }
+    public void deleteMedication(Long id) {
+        medicationRepository.deleteById(id);
+    }
+
 }
