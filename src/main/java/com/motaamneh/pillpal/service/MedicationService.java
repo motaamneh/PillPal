5b1edf519b005e7e7de302e469cdbc12d9445c90
motaamneh <mtaamneh292@gmail.com>
2025-06-30 15:10:46 +0300

    MedicationController + changes on the service

--- src/main/java/com/motaamneh/pillpal/service/MedicationService.java
+++ src/main/java/com/motaamneh/pillpal/service/MedicationService.java
@@ -1,7 +1,10 @@
 package com.motaamneh.pillpal.service;
 
 import com.motaamneh.pillpal.entity.Medication;
+import com.motaamneh.pillpal.entity.User;
 import com.motaamneh.pillpal.repository.MedicationRepository;
+import com.motaamneh.pillpal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,12 +12,19 @@ import java.util.Optional;
 
 @Service
 public class MedicationService {
-    private MedicationRepository medicationRepository;
-    public MedicationService(MedicationRepository medicationRepository) {
+    private final MedicationRepository medicationRepository;
+
+    private final UserRepository userRepository;
+    public MedicationService(MedicationRepository medicationRepository, UserRepository userRepository) {
         this.medicationRepository = medicationRepository;
+        this.userRepository = userRepository;
     }
 
-    public  Medication createMedication(Medication medication) {
+    public Medication createMedication(Long userId, Medication medication) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+
+        medication.setUser(user); // associate user
         return medicationRepository.save(medication);
     }
     public List<Medication> getAllMedications() {
