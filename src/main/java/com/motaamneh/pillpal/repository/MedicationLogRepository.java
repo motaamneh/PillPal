10334dd1258aaa0b580a2df240556b303a26d5d2
motaamneh <mtaamneh292@gmail.com>
2025-06-30 13:36:23 +0300

    MedicationLogService

--- src/main/java/com/motaamneh/pillpal/repository/MedicationLogRepository.java
+++ src/main/java/com/motaamneh/pillpal/repository/MedicationLogRepository.java
@@ -3,5 +3,13 @@ package com.motaamneh.pillpal.repository;
 import com.motaamneh.pillpal.entity.MedicationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MedicationLogRepository extends JpaRepository<MedicationLog, Long> {
+    List<MedicationLog> findByUserAndDate(Long userId, LocalDate date);
+
+    List<MedicationLog> findByUserId(Long userId);
+
+    boolean existsByScheduleIdAndDate(Long scheduleId, LocalDate date);
 }
--- src/main/java/com/motaamneh/pillpal/service/MedicationLogService.java
+++ src/main/java/com/motaamneh/pillpal/service/MedicationLogService.java
@@ -1,17 +1,36 @@
 package com.motaamneh.pillpal.service;
 
+import com.motaamneh.pillpal.entity.Medication;
+import com.motaamneh.pillpal.entity.MedicationLog;
 import com.motaamneh.pillpal.repository.MedicationLogRepository;
 import com.motaamneh.pillpal.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
 public class MedicationLogService {
     private final MedicationLogRepository medicationLogRepository;
-    private final UserRepository userRepository;
 
     public MedicationLogService(MedicationLogRepository medicationLogRepository, UserRepository userRepository) {
         this.medicationLogRepository = medicationLogRepository;
-        this.userRepository = userRepository;
+
+    }
+    public MedicationLog logMedication(MedicationLog medication) {
+        return medicationLogRepository.save(medication);
+    }
+    public List<MedicationLog> getLogsByUserAndDate(Long userId, LocalDate date) {
+        return medicationLogRepository.findByUserAndDate(userId,date);
+    }
+    public List<MedicationLog> getAllLogsForUser(Long userId) {
+        return medicationLogRepository.findByUserId(userId);
+    }
+    public boolean didUserLogToday(Long scheduleId, LocalDate date) {
+        return medicationLogRepository.existsByScheduleIdAndDate(scheduleId,date);
+    }
+    public void deleteLog(Long id){
+        medicationLogRepository.deleteById(id);
     }
 
 
