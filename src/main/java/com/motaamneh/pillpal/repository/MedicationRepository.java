5035319dc48e5c6c4222ede1b817e37d5e285a1b
motaamneh <mtaamneh292@gmail.com>
2025-06-30 13:07:30 +0300

    MedicationService

--- src/main/java/com/motaamneh/pillpal/repository/MedicationRepository.java
+++ src/main/java/com/motaamneh/pillpal/repository/MedicationRepository.java
@@ -3,7 +3,10 @@ package com.motaamneh.pillpal.repository;
 import com.motaamneh.pillpal.entity.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
 
 
+    List<Medication> findByUserId(Long userId);
 }
