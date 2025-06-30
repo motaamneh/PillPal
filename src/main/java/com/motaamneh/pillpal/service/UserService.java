dc8d6f9bb33708e3b6c7f3f4e81ef4c167bc69a3
motaamneh <mtaamneh292@gmail.com>
2025-06-30 12:58:32 +0300

    UserService

--- src/main/java/com/motaamneh/pillpal/service/UserService.java
+++ src/main/java/com/motaamneh/pillpal/service/UserService.java
@@ -1,7 +1,11 @@
 package com.motaamneh.pillpal.service;
 
+import com.motaamneh.pillpal.entity.User;
 import com.motaamneh.pillpal.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -10,4 +14,30 @@ public class UserService {
         this.userRepository = userRepository;
 
     }
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(Long id,User updatedUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(user.getName());
+                    user.setEmail(user.getEmail());
+                    user.setPassword(user.getPassword());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new RuntimeException("User is not found"));
+
+    }
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+
 }
